4339a801c28f9e0f263a7fe8e5c782b55f7386a2
Kekker-git <kekker.crane@gmail.com>
2017-10-31 14:34:49 -0400

    Added stuff to install script again.

---
+++ kekker/.anaconda/navigator/scripts/notebook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source /opt/anaconda/bin/activate /opt/anaconda
+/opt/anaconda/bin/jupyter-notebook >/home/kekker/.anaconda/navigator/scripts/notebook-out-1.txt 2>/home/kekker/.anaconda/navigator/scripts/notebook-err-1.txt
